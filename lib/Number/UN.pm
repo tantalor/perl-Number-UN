9278a5cd4d19b75c58e23c0c3a291e0d10914314
John Tantalo <john.tantalo@gmail.com>
2012-05-02 21:05:02 -0700

    Number::UN and tests

---
+++ lib/Number/UN.pm
@@ -0,0 +1,34 @@
+package Number::UN;
+
+use strict;
+use JSON::Syck;
+use Path::Class 'file';
+
+use Exporter 'import';
+our @EXPORT_OK = qw(get_un);
+
+=head1 NAME
+
+Number::UN
+
+=head1 SYNOPSIS
+
+  use Number::UN 'get_un';
+
+  my %un = get_un(1993);
+  print $un{description}; # Combustible liquids, n.o.s.
+
+=cut
+
+sub get_un {
+  my $data_filename = sprintf "%s/%04d.json", data_dir(), shift;
+  return unless -e $data_filename;
+  my $data = JSON::Syck::LoadFile($data_filename) or return;
+  return %$data;
+}
+
+sub data_dir {
+  file(__FILE__)->parent()->subdir('UN-data');  
+}
+
+1;
